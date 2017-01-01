883dc32e3f5116e49a65ce95899fc7295b090b11
Claudio Giordano <claudio.giordano@autistici.org>
2017-01-01 18:06:02 +0100

    Add build and test files

---
+++ tests/XlsExporterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace clagiordano\weblibs\dataexporter\tests;
+
+use clagiordano\weblibs\dataexporter\XlsExporter;
+
+/**
+ * Class XlsExporterTest
+ * @package clagiordano\weblibs\validator\tests
+ */
+class XlsExporterTest extends \PHPUnit_Framework_TestCase
+{
+}
