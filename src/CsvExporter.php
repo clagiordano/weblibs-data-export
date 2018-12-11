aec9fbe903b3cb587bd1b8168563ac10b994e3f8
Claudio Giordano <claudio.giordano@autistici.org>
2018-12-11 22:35:40 +0100

    Add csvExporter class

--- src/CsvExporter.php
+++ src/CsvExporter.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: claudio
- * Date: 11/12/18
- * Time: 22.19
- */
 
 namespace clagiordano\weblibs\dataexport;
 
-
-class CsvExporter
+/**
+ * Class CsvExporter
+ * @package clagiordano\weblibs\dataexport
+ */
+class CsvExporter extends AbstractExporter
 {
+    /**
+     * @param array $fields
+     * @param int $mode
+     * @param string $fieldSeparator
+     * @return bool|int
+     */
+    public function writeRow(array $fields, $mode = FILE_APPEND, $fieldSeparator = ';')
+    {
+        $outputString = join($fieldSeparator, $fields) . PHP_EOL;
 
+        return file_put_contents($this->fileName, $outputString, $mode);
+    }
 }
